f45aae0a3b342c6177ef1b1d72cf68ce5703223a
cent <changteng.qin.working@insighttracking.cn>
2025-03-17 12:30:36 +0800

    🐞 fix: capitalizeFirstLetter

--- src/generator.ts
+++ src/generator.ts
@@ -25,7 +25,7 @@ export async function generateTemplate(featureName: string, folderPath: string) 
 
 function generateScreenString(featureName: string): string {
     var _featureName = capitalizeFirstLetter(featureName);
-    return `import 'package:flutter/material.dart';\nimport 'package:provider/provider.dart';\n\nimport 'controller.dart';\n\nclass ${_featureName}Screen extends StatelessWidget {\n\tconst ${_featureName}Screen({super.key});\n\n\tstatic const routeName = "/${featureName}";\n\n\t@override\n\tWidget build(BuildContext context) {\n\t\treturn ChangeNotifierProvider(\n\t\t\tcreate: (BuildContext context) => ${_featureName}Controller(),\n\t\t\tchild: _buildPage(),\n\t\t);\n\t}\n\n\tWidget _buildPage() {\n\t\treturn Scaffold(\n\t\t\tappBar: AppBar(\n\t\t\t\ttitle: const Text("${_featureName}"),\n\t\t\t),\n\t\t\tbody: Consumer<${_featureName}Controller>(\n\t\t\t\tbuilder: (context, controller, child) {\n\t\t\t\t\treturn Center(\n\t\t\t\t\t\tchild: Text("\${controller.count}"),\n\t\t\t\t\t);\n\t\t\t\t},\n\t\t\t),\n\t\t);\n\t}\n}`;
+    return `import 'package:flutter/material.dart';\nimport 'package:provider/provider.dart';\n\nimport 'controller.dart';\n\nclass ${_featureName}Screen extends StatelessWidget {\n\tconst ${_featureName}Screen({super.key});\n\n\tstatic const routeName = "/${featureName}";\n\n\t@override\n\tWidget build(BuildContext context) {\n\t\treturn ChangeNotifierProvider(\n\t\t\tcreate: (BuildContext context) => ${_featureName}Controller()..init(),\n\t\t\tchild: _buildPage(),\n\t\t);\n\t}\n\n\tWidget _buildPage() {\n\t\treturn Scaffold(\n\t\t\tappBar: AppBar(\n\t\t\t\ttitle: const Text("${_featureName}"),\n\t\t\t),\n\t\t\tbody: Consumer<${_featureName}Controller>(\n\t\t\t\tbuilder: (context, controller, child) {\n\t\t\t\t\treturn Center(\n\t\t\t\t\t\tchild: Text("\${controller.count}"),\n\t\t\t\t\t);\n\t\t\t\t},\n\t\t\t),\n\t\t);\n\t}\n}`;
 }
 
 function generateIndexString(): string {
@@ -34,9 +34,13 @@ function generateIndexString(): string {
 
 function generateControllerString(featureName: string): string {
     var _featureName = capitalizeFirstLetter(featureName);
-    return `import 'package:flutter/material.dart';\n\nclass ${_featureName}Controller extends ChangeNotifier {\n\tint _count = 0;\n\n\tint get count => _count;\n\n\tvoid increment() {\n\t\t_count++;\n\t\tnotifyListeners();\n\t}\n}`;
+    return `import 'package:flutter/material.dart';\n\nclass ${_featureName}Controller extends ChangeNotifier {\n  bool _isReady = false;\n  bool get ready => _isReady;\n\n  Future<void> init() async {\n    // The page needs to be displayed before it can be displayed\n    await Future.delayed(const Duration(seconds: 1)).then((_) => _isReady = true);\n    notifyListeners();\n  }\n}`;
 }
 
 function capitalizeFirstLetter(input: string): string {
-    return input.charAt(0).toUpperCase() + input.slice(1);
+    return input
+        .toLowerCase()
+        .replace(/(?:^|_)(\w)/g, (match, p1, index) =>
+            index === 0 ? p1.toUpperCase() : p1.toLowerCase()
+        );
 }
